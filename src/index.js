cba28e50fa42c5a41ee3c2224bcebf7bde774f17
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-01-14 19:22:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom";
+import store from "./redux/redux-store.js";
+import { Provider } from "react-redux";
+import App from "./App.js";
+import "./index.css";
 
-// import * as serviceWorker from './serviceWorker';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from "./serviceWorker";
 
-import {BrowserRouter} from 'react-router-dom';
-import store from './redux/redux_store.js';
-
-
-let rerenderEntireTree = (state) => {
-	ReactDOM.render(
-		<BrowserRouter>
-			<App 
-				state={state} 
-				dispatch={store.dispatch.bind(store)}
-				store={store}  /> 
-		</BrowserRouter>, 
-		document.getElementById('root')
-	);
-}
-debugger;
-
-rerenderEntireTree(store.getState());
-
-store.subscribe(() => {
-	let state = store.getState();
-	rerenderEntireTree(state);
-});
-
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
