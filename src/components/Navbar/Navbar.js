f1f994cc5f3389ed28c2444ef29a656629aec179
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-01-14 19:20:45 +0300

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import classes from './Navbar.module.css';
-import {NavLink} from 'react-router-dom';
-
+import React from "react";
+import classes from "./Navbar.module.css";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-	return (
-		<nav className={classes.nav}>
-			<div  className={`${classes.item} ${classes.active}`}>
-			  	<NavLink to="/profile">Profile</NavLink>
-			</div>
-			<div  className={classes.item}>
-			  	<NavLink to="/dialogs">Messages</NavLink>
-			</div>
-			
-		</nav>
-	);
-}
+  return (
+    <nav className={classes.nav}>
+      <div className={classes.item}>
+        <NavLink to="/profile" activeClassName={classes.active}>
+          Profile
+        </NavLink>
+      </div>
+
+      <div className={classes.item}>
+        <NavLink to="/dialogs" activeClassName={classes.active}>
+          Messages
+        </NavLink>
+      </div>
+
+      <div className={classes.item}>
+        <NavLink to="/users" activeClassName={classes.active}>
+          Users
+        </NavLink>
+      </div>
+
+      <div className={classes.item}>
+        <NavLink to="/news" activeClassName={classes.active}>
+          News
+        </NavLink>
+      </div>
+
+      <div className={classes.item}>
+        <NavLink to="/settings" activeClassName={classes.active}>
+          Settings
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
 export default Navbar;
