fac6ccd9a275ee81551e7b8878e2ccec2f2a8364
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-01-14 19:21:27 +0300

    Update MyPosts.js

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -1,45 +1,45 @@
-import React from 'react';
-import classes from './MyPosts.module.css';
-import Post from './Post/Post.js';
+import React from "react";
+import classes from "./MyPosts.module.css";
+import Post from "./Post/Post.js";
 
+const MyPosts = props => {
+  debugger;
+  // let posts = props.state.profileReducer.postsData;
 
-const MyPosts = (props) => {
-	let postsElement = props.postData.map( 
-		post => <Post message={post.message} likesCount={post.likesCount} /> );
-
-	let newPostElement = React.createRef();
-
-	let onAddPost = () => {
-		props.addPost();
-		// props.dispatch(addPostActionCreator());
-	}
-
-	let onPostChange = () => {
-		let text = newPostElement.current.value;
-		props.updateNewPostText(text);
-	}
-
-	
-	return (
-		<div className={classes.postsBlock}>
-			<h3>My posts</h3>
-			<div>
-				<div>
-					<textarea 
-						ref={newPostElement} 
-						onChange={onPostChange}
-						value={props.newPostText} />
-				</div>
-
-				<div>
-					<button onClick={onAddPost}>Add post</button>
-				</div>
-			</div>
-			<div className={classes.posts}>
-				{postsElement}
-			</div>
-		</div>
-	);
-}
+  let newPost = () => {
+    props.addPost();
+  };
+  let onPostChange = event => {
+    let text = event.target.value;
+    props.addPostChange(text);
+  };
+  let removePost = id => {
+    props.removePost(id);
+  };
+  return (
+    <div className={classes.postsBlock}>
+      <h3>My posts</h3>
+      <div>
+        <div>
+          <textarea value={props.editChangePost} onChange={onPostChange} />
+        </div>
+        <div>
+          <button onClick={newPost}>Add post</button>
+        </div>
+      </div>
+      <div className={classes.posts}>
+        {props.posts.map(({ id, post, likesCount }) => (
+          <Post
+            removePost={removePost}
+            key={id}
+            id={id}
+            post={post}
+            likesCount={likesCount}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default MyPosts;
