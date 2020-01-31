3911b6d38a2cd811855ff6ff6df2279cec5f2fc7
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-01-31 12:17:53 +0300

    Create Preloader.js

---
+++ src/components/common/Preloader/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+import preloader from "../../../assets/images/preloader.gif";
+
+let Preloader = props => {
+  return (
+    <>
+      <img src={preloader} alt="Прелоадер" />
+    </>
+  );
+};
+export default Preloader;
